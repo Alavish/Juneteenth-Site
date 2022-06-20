7e626774192d130c53a91293c9e211c7e05173fe
Alavish <achristopher22@my.whitworth.edu>
2022-06-20 12:43:22 -0700

    change

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
   },
 
   telemetry: false,
-  target: 'serverless',
+  target: 'static',
   // ssr: false, // This should only be false if you don't want the HTML to render.
   dir: {
     layouts: `entries/${name}/layouts`,
